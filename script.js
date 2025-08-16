2ce723e03b5580165012759810ba3251afa86681
yutaro_git <yutaro96.adm@gmail.com>
2025-08-16 22:50:32 +0900

    resolve conflicts during rebase

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
+<<<<<<< HEAD
     const taskInput = document.getElementById('new-task-input');
+=======
+    const taskNameInput = document.getElementById('new-task-name');
+    const taskProgressSelect = document.getElementById('new-task-progress');
+    const taskAssigneeInput = document.getElementById('new-task-assignee');
+>>>>>>> 6452606 (first clean commit)
     const addTaskButton = document.getElementById('add-task-button');
     const todoList = document.getElementById('todo-list');
     const doingList = document.getElementById('doing-list');
@@ -15,12 +21,26 @@ document.addEventListener('DOMContentLoaded', function () {
         tasks.forEach(task => {
             const taskElement = document.createElement('div');
             taskElement.classList.add('task');
+<<<<<<< HEAD
             taskElement.textContent = task.text;
+=======
+>>>>>>> 6452606 (first clean commit)
             taskElement.draggable = true;
             taskElement.dataset.id = task.id;
 
             taskElement.addEventListener('dragstart', dragStart);
 
+<<<<<<< HEAD
+=======
+            const taskName = document.createElement('p');
+            taskName.textContent = `タスク名: ${task.name}`;
+            taskElement.appendChild(taskName);
+
+            const taskAssignee = document.createElement('p');
+            taskAssignee.textContent = `担当者: ${task.assignee}`;
+            taskElement.appendChild(taskAssignee);
+
+>>>>>>> 6452606 (first clean commit)
             if (task.status === 'todo') {
                 todoList.appendChild(taskElement);
             } else if (task.status === 'doing') {
@@ -32,17 +52,35 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function addTask() {
+<<<<<<< HEAD
         const taskText = taskInput.value.trim();
         if (taskText !== '') {
             const task = {
                 id: Date.now(),
                 text: taskText,
                 status: 'todo'
+=======
+        const taskName = taskNameInput.value.trim();
+        const taskProgress = taskProgressSelect.value;
+        const taskAssignee = taskAssigneeInput.value.trim();
+
+        if (taskName !== '') {
+            const task = {
+                id: Date.now(),
+                name: taskName,
+                status: taskProgress,
+                assignee: taskAssignee
+>>>>>>> 6452606 (first clean commit)
             };
             tasks.push(task);
             localStorage.setItem('tasks', JSON.stringify(tasks));
             renderTasks();
+<<<<<<< HEAD
             taskInput.value = '';
+=======
+            taskNameInput.value = '';
+            taskAssigneeInput.value = '';
+>>>>>>> 6452606 (first clean commit)
         }
     }
 
